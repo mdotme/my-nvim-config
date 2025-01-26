c04e0c6c17b6f978977760e773d849877049b993
Manuchehr <manuchekhr.3232@gmail.com>
2025-01-26 16:40:51 +0500

    Add: testing (vitest) & tmux-navigator

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", "<C-p>", "<cmd>Telescope find_files<cr>", { desc = "Find files (Telescope)" })
+map("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Search string live_grep (global)" })
+map(
+  "n",
+  "<leader>fw",
+  "<cmd>Telescope grep_string<cr>",
+  { desc = "Search string under the cursor gre_string (global)" }
+)
+
+-- Normal mode: Replace the word under the cursor and go into insert mode
+map("n", "<leader>cw", "*Ncgn", { noremap = true, silent = true, desc = "Change word under cursor" })
+
+-- Visual mode: Replace the visually selected text and go into insert mode
+map("v", "<leader>cw", "y/<C-r>0<CR>Ncgn", { noremap = true, silent = true, desc = "Change selected text" })
+
+map("n", "<C-w>", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
---
+++ lua/plugins/project.lua
@@ -0,0 +1,7 @@
+return {
+  "ahmedkhalf/project.nvim",
+  opts = {
+    manual_mode = false,
+    pattern = { ".git", "package.json" },
+  },
+}
